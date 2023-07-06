835a2641c11e2360619673a44b6487709f364702
Matt Alton <mattalton97@gmail.com>
2023-07-06 22:28:57 +0100

    Setup backend framework with Express.js

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import axios from "axios";
 
+const message = ref<string>("");
+
+axios
+  .get("/api/")
+  .then((response) => {
+    // Handle the response data
+    message.value = response.data;
+    // ...
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.error(error);
+  });
 </script>
 
 <template>
   <div class="mb-4">
-    <img src="../assets/img/kanji-cards-light.webp" class="logo" alt="Kanji Cards Logo" />
+    <img
+      src="../assets/img/kanji-cards-light.webp"
+      class="logo"
+      alt="Kanji Cards Logo"
+    />
   </div>
 
   <h1>Welcome!</h1>
+  <h2>{{ message }}</h2>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-import './assets/scss/styles.scss'
-import 'bootstrap-icons/font/bootstrap-icons.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import {createPinia} from "pinia";
+import "./assets/scss/styles.scss";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
-app.use(router)
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+app.mount("#app");
