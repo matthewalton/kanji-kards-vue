c467703d6e03e657b0352e06e2440e54ab978c00
Matt Alton <mattalton97@gmail.com>
2023-07-17 21:38:16 +0100

    Fixed MySQL crashing on startup

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,42 +4,35 @@ const path = require("path");
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "kanji-cards-database-1",
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, // Adjust the limit based on your needs
+  queueLimit: 0,
 });
 
-function getPacks(req, res) {
-  try {
-    connection.query("SELECT * FROM kanji_categories", (error, results) => {
-      if (error) {
-        res.send(error);
-        return;
-      }
+function getPacks(req, res, next) {
+  pool.query("SELECT * FROM kanji_categories", (error, results) => {
+    if (error) {
+      return next(error);
+    }
 
-      res.send(results);
-    });
-  } catch (error) {
-    res.send(error);
-  }
+    res.send(results);
+  });
 }
 
-function getCards(req, res) {
-  try {
-    connection.query("SELECT * FROM kanji_characters", (error, results) => {
-      if (error) {
-        res.send(error);
-        return;
-      }
+function getCards(req, res, next) {
+  pool.query("SELECT * FROM kanji_characters", (error, results) => {
+    if (error) {
+      return next(error);
+    }
 
-      res.send(results);
-    });
-  } catch (error) {
-    res.send(error);
-  }
+    res.send(results);
+  });
 }
 
 module.exports = { getPacks, getCards };
